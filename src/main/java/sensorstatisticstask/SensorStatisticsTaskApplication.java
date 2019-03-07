7f01c107c20142a095a5fc8484a6b609a3465573
Groksoar <groksoar@gmail.com>
2019-03-07 21:13:35 +0100

    Initial commit

---
+++ src/main/java/sensorstatisticstask/SensorStatisticsTaskApplication.java
@@ -0,0 +1,14 @@
+package sensorstatisticstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SensorStatisticsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorStatisticsTaskApplication.class, args);
+	}
+
+}
