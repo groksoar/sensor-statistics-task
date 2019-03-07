1584f7e7720bea8a8db62586cce4d6c4c02bd44c
Groksoar <groksoar@gmail.com>
2019-03-07 22:38:05 +0100

    Sorted statistics report

--- src/main/java/sensorstatisticstask/component/StatisticsCalculatorImpl.java
+++ src/main/java/sensorstatisticstask/component/StatisticsCalculatorImpl.java
@@ -6,15 +6,14 @@ import sensorstatisticstask.entity.StatisticsReport;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Map.Entry;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summarizingInt;
 
@@ -50,10 +49,27 @@ public class StatisticsCalculatorImpl implements StatisticsCalculator {
                             (k, v) -> v == null ? sensorMeasurement.isFailed() : v && sensorMeasurement.isFailed()))
                     .collect(groupingBy(SensorMeasurement::getSensorId, summarizingInt(SensorMeasurement::getAdjustedMeasurement)));
 
-            return new StatisticsReport(files.size(), numOfMeasurements.sum(), numOfFailedMeasurements.sum(), map);
+            Set<String> failedSensors = failedSensorsTracker.entrySet().stream()
+                    .filter(Entry::getValue)
+                    .map(Entry::getKey)
+                    .collect(Collectors.toSet());
+
+            map = sortStatistics(map, failedSensors);
+
+            return new StatisticsReport(files.size(), numOfMeasurements.sum(), numOfFailedMeasurements.sum(), map, failedSensors);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private Map<String, IntSummaryStatistics> sortStatistics(Map<String, IntSummaryStatistics> statistics, Set<String> failedSensors) {
+        return statistics.entrySet().stream()
+                .sorted((e1, e2) -> {
+                    double d1 = failedSensors.contains(e1.getKey()) ? Double.MIN_VALUE : e1.getValue().getAverage();
+                    double d2 = failedSensors.contains(e2.getKey()) ? Double.MIN_VALUE : e2.getValue().getAverage();
+                    return Double.compare(d2, d1);
+                })
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (s1, s2) -> s1, LinkedHashMap::new));
     }
 
 }
--- src/main/java/sensorstatisticstask/entity/StatisticsReport.java
+++ src/main/java/sensorstatisticstask/entity/StatisticsReport.java
@@ -2,18 +2,21 @@ package sensorstatisticstask.entity;
 
 import java.util.IntSummaryStatistics;
 import java.util.Map;
+import java.util.Set;
 
 public class StatisticsReport {
     private long numOfProcessedFiles;
     private long numOfProcessedMeasurements;
     private long numOfFailedMeasurements;
     private Map<String, IntSummaryStatistics> statistics;
+    private Set<String> failedSensors;
 
-    public StatisticsReport(long numOfProcessedFiles, long numOfProcessedMeasurements, long numOfFailedMeasurements, Map<String, IntSummaryStatistics> statistics) {
+    public StatisticsReport(long numOfProcessedFiles, long numOfProcessedMeasurements, long numOfFailedMeasurements, Map<String, IntSummaryStatistics> statistics, Set<String> failedSensors) {
         this.numOfProcessedFiles = numOfProcessedFiles;
         this.numOfProcessedMeasurements = numOfProcessedMeasurements;
         this.numOfFailedMeasurements = numOfFailedMeasurements;
         this.statistics = statistics;
+        this.failedSensors = failedSensors;
     }
 
     public long getNumOfProcessedFiles() {
@@ -32,4 +35,7 @@ public class StatisticsReport {
         return statistics;
     }
 
+    public Set<String> getFailedSensors() {
+        return failedSensors;
+    }
 }
